5722f8ec46554bf7e9e6222852e15903fb645456
thiagoherculano <61213880+thiagoherculano@users.noreply.github.com>
2021-09-13 21:40:42 -0300

    Add files via upload
    Abre a pagina do github e faz o login automático

---
+++ index.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+navegador = webdriver.Chrome()
+navegador.maximize_window()
+navegador.get('https://github.com/login')
+
+Username_login = 'thiagoherculan@gmail.com'
+Senha_login = 'Thiago1820'
+
+campo_username = navegador.find_element_by_css_selector('#login_field')
+sleep(1)
+campo_username.send_keys(Username_login)
+campo_senha = navegador.find_element_by_css_selector('#password')
+sleep(2)
+campo_senha.send_keys(Senha_login)
+sleep(2)
+
+navegador.find_element_by_xpath('//*[@id="login"]/div[4]/form/div/input[12]').click()
+
+
+
